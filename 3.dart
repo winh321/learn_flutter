7d34dce7facdf8bbcd5d8aa978e024404c2cf6ff
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-15 14:29:28 +0630

    Create 3.dart

---
+++ 3.dart
@@ -0,0 +1,22 @@
+add stateless widget to get hot reload
+
+  stless - keyword
+
+  class Second extends StatelessWidget {
+  const Second({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+usage 
+
+  void main(){
+  return runApp(MaterialApp(
+      home:Home()
+  ));
+}
