cf4dd25a20119b013dcde0854110e0055b31c655
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-15 13:43:38 +0630

    Create 2.dart

---
+++ 2.dart
@@ -0,0 +1,21 @@
+add font
+
+1. download 
+2. paste in fonts folder
+3. set insert by following
+    fonts:
+     - family: IndieFlower
+       fonts:
+         - asset: fonts/IndieFlower-Regular.ttf
+4. Use 
+           body:Center(
+          child: Text("Hi brother yay",style:TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'IndieFlower',
+
+
+          )) ,
+        ),
