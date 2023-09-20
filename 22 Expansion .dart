db5e65b6513f48d28701dd4b7ed77cf9737fec92
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-20 14:15:45 +0630

    Create 22 Expansion .dart

---
+++ 22 Expansion .dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [ExpansionTile].
+
+void main() => runApp(const ExpansionTileApp());
+
+class ExpansionTileApp extends StatelessWidget {
+  const ExpansionTileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ExpansionTile Sample')),
+        body: const ExpansionTileExample(),
+      ),
+    );
+  }
+}
+
+class ExpansionTileExample extends StatefulWidget {
+  const ExpansionTileExample({super.key});
+
+  @override
+  State<ExpansionTileExample> createState() => _ExpansionTileExampleState();
+}
+
+class _ExpansionTileExampleState extends State<ExpansionTileExample> {
+  bool _customTileExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const ExpansionTile(
+          title: Text('ExpansionTile 1'),
+          subtitle: Text('Trailing expansion arrow icon'),
+          children: <Widget>[
+            ListTile(title: Text('This is tile number 1')),
+          ],
+        ),
+        ExpansionTile(title: Text("HI"),
+          subtitle: Text("SubTitle"),
+          children: [
+            MaterialButton(onPressed: null, child: Text("click me"),),
+            Image.network('https://picsum.photos/250?image=1'),
+          ],
+        )
+      ],
+    );
+  }
+}
