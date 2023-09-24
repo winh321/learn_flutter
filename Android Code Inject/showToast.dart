ab90182c5612634933c2d4014be9adf1c3fded67
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-24 11:56:31 +0630

    Create showToast.dart

---
+++ Android Code Inject/showToast.dart
@@ -0,0 +1,28 @@
+in dart
+
+    var channel = MethodChannel("babadevs");
+  showToast(){
+    channel.invokeMethod("showToast");
+  }
+
+in kotlin
+
+  package com.example.xxx.exx
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val channelName = "babadevs"
+    override fun configureFlutterEngine(flutterEngine : FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channelName)
+        channel.setMethodCallHandler{ call,result->
+            if (call.method == "showToast"){
+                Toast.makeText(this,"MY Toast",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
