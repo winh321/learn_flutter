e59c42908f067c55d275bae8c93f260d81848d84
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-24 12:24:58 +0630

    Create Native data to Dart.dart

---
+++ Android Code Inject/Native data to Dart.dart
@@ -0,0 +1,47 @@
+in dart
+
+
+class _HomeState extends State<Home> {
+  String data = "Click";
+
+  var channel = MethodChannel("GETT");
+  getData() async {
+    data = await channel.invokeMethod("getData");
+    setState(() {
+
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: getData,
+        child: Text(data),
+      ),
+    );
+  }
+}
+
+in kotlin
+
+package com.example.xxx.exx
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val channelName = "GETT"
+    override fun configureFlutterEngine(flutterEngine : FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channelName)
+        channel.setMethodCallHandler{ call,result->
+            if (call.method == "getData"){
+                result.success("Love");
+            }
+        }
+    }
+}
