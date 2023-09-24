287ac7bc71a208509f1bcb088ba083053de22f97
winh321 <69839416+winh321@users.noreply.github.com>
2023-09-24 12:04:05 +0630

    Create message dart to kotlin.dart

---
+++ Android Code Inject/message dart to kotlin.dart
@@ -0,0 +1,29 @@
+in dart
+  
+var channel = MethodChannel("babadevs");
+  showToast(){
+    channel.invokeMethod("showToast",{"message":"Message From Dart"});
+  }
+
+in java
+
+package com.example.xxx.exx
+import android.widget.Toast
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity: FlutterActivity() {
+    private val channelName = "babadevs"
+    override fun configureFlutterEngine(flutterEngine : FlutterEngine){
+        super.configureFlutterEngine(flutterEngine)
+
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger,channelName)
+        channel.setMethodCallHandler{ call,result->
+            var args = call.arguments as Map<String,String>
+            if (call.method == "showToast"){
+                Toast.makeText(this,args["message"],Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
